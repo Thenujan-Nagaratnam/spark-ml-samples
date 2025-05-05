5444ea5ebc51caa1516a4c442ec717b2968df43a
Thenujan-Nagaratnam <thenujanthenu22@gmail.com>
2025-05-05 22:18:19 +0530

    bug fix

--- spark-driver/src/main/java/com/lohika/morning/ml/spark/driver/service/lyrics/pipeline/CommonLyricsPipeline.java
+++ spark-driver/src/main/java/com/lohika/morning/ml/spark/driver/service/lyrics/pipeline/CommonLyricsPipeline.java
@@ -18,6 +18,10 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.when;
+import static org.apache.spark.sql.functions.lit;
+
 
 public abstract class CommonLyricsPipeline implements LyricsPipeline {
 
