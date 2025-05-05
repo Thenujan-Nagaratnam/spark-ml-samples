1c8858d7bf2e10341a8567c2aca19f2180486c15
Thenujan-Nagaratnam <thenujanthenu22@gmail.com>
2025-05-05 21:34:33 +0530

    bug fix

--- api/src/main/java/com/lohika/morning/ml/api/service/ProductService.java
+++ api/src/main/java/com/lohika/morning/ml/api/service/ProductService.java
@@ -1,25 +1,25 @@
-package com.lohika.morning.ml.api.service;
+// package com.lohika.morning.ml.api.service;
 
-import com.lohika.morning.ml.spark.driver.service.product.ProductMLService;
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+// import com.lohika.morning.ml.spark.driver.service.product.ProductMLService;
+// import java.util.Map;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class ProductService {
+// @Component
+// public class ProductService {
 
-    @Autowired
-    private ProductMLService productMLService;
+//     @Autowired
+//     private ProductMLService productMLService;
 
-    @Value("${product.training.set.csv.file.path}")
-    private String productTrainingSetCsvFilePath;
+//     @Value("${product.training.set.csv.file.path}")
+//     private String productTrainingSetCsvFilePath;
 
-    @Value("${product.test.set.csv.file.path}")
-    private String productTestSetCsvFilePath;
+//     @Value("${product.test.set.csv.file.path}")
+//     private String productTestSetCsvFilePath;
 
-    public Map<String, Object> trainRandomForestClassifier() {
-        return productMLService.trainRandomForestClassifier(productTrainingSetCsvFilePath, productTestSetCsvFilePath);
-    }
+//     public Map<String, Object> trainRandomForestClassifier() {
+//         return productMLService.trainRandomForestClassifier(productTrainingSetCsvFilePath, productTestSetCsvFilePath);
+//     }
 
-}
+// }
